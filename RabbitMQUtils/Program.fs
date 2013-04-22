3b5d7692e3f8e93218ecc7b00c7ec9956bc481d3
Fawad Halim <fawad@fawad.net>
2013-04-22 15:14:15 -0500

    Slight refactorifying.

--- RabbitMQUtils/Program.fs
+++ RabbitMQUtils/Program.fs
@@ -16,13 +16,17 @@ let listResources (client:RestClient) resourceType =
 let deleteResource (client:RestClient) resourceType (resources:RabbitMQResource seq) =
     resources |> Seq.iter (fun resource -> client.Execute (new RestRequest(resource.url resourceType, Method.DELETE)) |> ignore)
 
+let filterDevelopmentObjects = Seq.filter (fun (x:RabbitMQResource) -> Seq.exists (fun y-> x.name.StartsWith y) ["NServiceBus"; "VideoStore"])
+
 [<EntryPoint>]
 let main argv = 
     let client = new RestClient "http://localhost:15672"
     client.Authenticator <- new HttpBasicAuthenticator("guest", "guest")
     (* Delete all exchanges with names starting with "NServiceBus" *)
     let exchanges = listResources client "exchanges"
-    exchanges.Data |> Seq.filter (fun x -> x.name.StartsWith "NServiceBus") |> deleteResource client "exchanges"
+    exchanges.Data |> filterDevelopmentObjects |> deleteResource client "exchanges"
     (* Print all queue names *)
     (listResources client "queues").Data |> Seq.map (fun x-> x.name) |> Seq.iter (printfn "%s")
+    let queues = listResources client "queues"
+    queues.Data |> filterDevelopmentObjects |> deleteResource client "queues"
     0 // return an integer exit code
