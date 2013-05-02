94fe5aa67218a904daf3edc55048fbb1e9ead229
Fawad Halim <fawad@fawad.net>
2013-05-02 15:00:43 -0500

    Added ability to create simple federation.

--- RabbitMQUtils/Program.fs
+++ RabbitMQUtils/Program.fs
@@ -2,6 +2,7 @@
 open RestSharp
 
 let e = Uri.EscapeDataString
+let json = "application/json"
 
 type RabbitMQResource() =
     member val name = "" with get, set
@@ -16,17 +17,35 @@ let listResources (client:RestClient) resourceType =
 let deleteResource (client:RestClient) resourceType (resources:RabbitMQResource seq) =
     resources |> Seq.iter (fun resource -> client.Execute (new RestRequest(resource.url resourceType, Method.DELETE)) |> ignore)
 
-let filterDevelopmentObjects = Seq.filter (fun (x:RabbitMQResource) -> Seq.exists (fun y-> x.name.StartsWith y) ["NServiceBus"; "VideoStore"])
+let filterDevelopmentObjects = Seq.filter (fun (x:RabbitMQResource) -> Seq.exists (fun y-> x.name.StartsWith y) ["subscriber";"ADP."; "Sales."; "Shipping."; "MvcApplication";"Messages"; "System"; "Billing" ; "CRM"; "LeadManager"])
 
-[<EntryPoint>]
-let main argv = 
-    let client = new RestClient "http://localhost:15672"
-    client.Authenticator <- new HttpBasicAuthenticator("guest", "guest")
-    (* Delete all exchanges with names starting with "NServiceBus" *)
+let clearOutQueues client =
     let exchanges = listResources client "exchanges"
     exchanges.Data |> filterDevelopmentObjects |> deleteResource client "exchanges"
-    (* Print all queue names *)
     (listResources client "queues").Data |> Seq.map (fun x-> x.name) |> Seq.iter (printfn "%s")
     let queues = listResources client "queues"
     queues.Data |> filterDevelopmentObjects |> deleteResource client "queues"
+
+let createDefaultUpstream (client:RestClient) (hostname:string) =
+    (new RestRequest("/api/parameters/federation-upstream/%2f/host_" + hostname.Replace(".", "_"), Method.PUT)).AddParameter(json, sprintf "{\"value\":{\"uri\":\"amqp://guest:guest@%s\"}}" hostname, ParameterType.RequestBody)
+        |>  client.Execute |> ignore
+
+let setupFederation auth =
+    let centralBrokerClient = new RestClient "http://192.168.116.205:15672"
+    centralBrokerClient.Authenticator <- auth
+    createDefaultUpstream centralBrokerClient "192.168.116.215"
+    (new RestRequest("/api/policies/%2f/fawad-fed", Method.PUT)).AddParameter(json, "{\"pattern\":\"^federated\.\", \"definition\": {\"federation-upstream-set\":\"all\"}}", ParameterType.RequestBody)
+        |>  centralBrokerClient.Execute |> ignore
+    0
+
+[<EntryPoint>]
+let main argv = 
+    let auth = new HttpBasicAuthenticator("guest", "guest")
+    let client = new RestClient "http://localhost:15672"
+    client.Authenticator <- auth
+    
+    //clearOutQueues client
+
+    setupFederation auth
+
     0 // return an integer exit code
