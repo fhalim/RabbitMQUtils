85916f3324767b7f796b9ea026e22c28a19c66e0
Fawad Halim <fawad@fawad.net>
2013-05-12 22:12:59 -0500

    Preliminary support for spinning up a cluster.

---
+++ RabbitMQUtils/ClusterAdmin.fs
@@ -0,0 +1,33 @@
+﻿module ClusterAdmin
+open System.Diagnostics
+open System.IO
+
+let rabbitMqHome = @"c:\Program Files (x86)\RabbitMQ Server\rabbitmq_server-3.1.0"
+
+type node = { name:string; port:int; adminPort:int}
+type cluster = {nodes:node seq;}
+
+let simpleNodeStrategy idx = {name = sprintf "rabbitmq_%i@%s" idx System.Environment.MachineName ; port = 5673 + idx; adminPort = 15673 + idx}
+
+
+let startNode nodeInfo =
+    let sbinDir = Path.Combine(rabbitMqHome, "sbin")
+    let startInfo = new ProcessStartInfo(Path.Combine(sbinDir, "rabbitmq-server.bat"), "")
+    Map([
+        ("RABBITMQ_NODENAME", nodeInfo.name);
+        ("RABBITMQ_NODE_PORT", string nodeInfo.port);
+        ("RABBITMQ_SERVER_START_ARGS", sprintf "-detached -rabbitmq_management listener [{{port,%i}}]" nodeInfo.adminPort)
+        ])
+        |> Map.iter (fun k v -> startInfo.EnvironmentVariables.Add(k, v))
+    startInfo.UseShellExecute <- false
+    startInfo.WorkingDirectory <- sbinDir
+    startInfo.RedirectStandardOutput <- true
+    startInfo.RedirectStandardError <- true
+    let proc = Process.Start(startInfo)
+    let stdOut = proc.StandardOutput.ReadToEnd()
+    let stdErr = proc.StandardError.ReadToEnd()
+    proc
+
+let createCluster count nodestrategy =
+    let nodeInfos = seq {for idx in 1..count do yield nodestrategy idx}
+    Seq.map startNode nodeInfos
--- RabbitMQUtils/Program.fs
+++ RabbitMQUtils/Program.fs
@@ -1,42 +1,6 @@
-﻿open System
+﻿module main
 open RestSharp
-
-let e = Uri.EscapeDataString
-let json = "application/json"
-
-type RabbitMQResource() =
-    member val name = "" with get, set
-    member val vhost = "" with get, set
-
-    member this.url resourceType =
-        "/api/" + resourceType + "/" + e(this.vhost) + "/" + e(this.name)
-
-let listResources (client:RestClient) resourceType =
-    client.Execute<ResizeArray<RabbitMQResource>>(new RestRequest ("/api/" + resourceType))
-
-let deleteResource (client:RestClient) resourceType (resources:RabbitMQResource seq) =
-    resources |> Seq.iter (fun resource -> client.Execute (new RestRequest(resource.url resourceType, Method.DELETE)) |> ignore)
-
-let filterDevelopmentObjects = Seq.filter (fun (x:RabbitMQResource) -> Seq.exists (fun y-> x.name.StartsWith y) ["subscriber";"ADP."; "Sales."; "Shipping."; "MvcApplication";"Messages"; "System"; "Billing" ; "CRM"; "LeadManager"])
-
-let clearOutQueues client =
-    let exchanges = listResources client "exchanges"
-    exchanges.Data |> filterDevelopmentObjects |> deleteResource client "exchanges"
-    (listResources client "queues").Data |> Seq.map (fun x-> x.name) |> Seq.iter (printfn "%s")
-    let queues = listResources client "queues"
-    queues.Data |> filterDevelopmentObjects |> deleteResource client "queues"
-
-let createDefaultUpstream (client:RestClient) (hostname:string) =
-    (new RestRequest("/api/parameters/federation-upstream/%2f/host_" + hostname.Replace(".", "_"), Method.PUT)).AddParameter(json, sprintf "{\"value\":{\"uri\":\"amqp://guest:guest@%s\"}}" hostname, ParameterType.RequestBody)
-        |>  client.Execute |> ignore
-
-let setupFederation auth =
-    let centralBrokerClient = new RestClient "http://192.168.116.205:15672"
-    centralBrokerClient.Authenticator <- auth
-    createDefaultUpstream centralBrokerClient "192.168.116.215"
-    (new RestRequest("/api/policies/%2f/fawad-fed", Method.PUT)).AddParameter(json, "{\"pattern\":\"^federated\.\", \"definition\": {\"federation-upstream-set\":\"all\"}}", ParameterType.RequestBody)
-        |>  centralBrokerClient.Execute |> ignore
-    0
+open ResourceAdmin
 
 [<EntryPoint>]
 let main argv = 
@@ -44,8 +8,9 @@ let main argv =
     let client = new RestClient "http://localhost:15672"
     client.Authenticator <- auth
     
-    //clearOutQueues client
-
-    setupFederation auth
+    //ResourceAdmin.clearOutQueues client
+    let processes = ClusterAdmin.createCluster 2 ClusterAdmin.simpleNodeStrategy
+    Seq.iter (fun _ -> ()) processes
+    //ResourceAdmin.setupFederation auth  |> ignore
 
     0 // return an integer exit code
